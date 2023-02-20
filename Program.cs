666029f1a9605239fc7b1b5030183c56ab0bf37d
Олег Виноградов <oleg.vinogradov@informicus.ru>
2023-02-21 01:41:35 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ЦПИбв_31_Виноградов_Олег_Танки
+{
+    static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
